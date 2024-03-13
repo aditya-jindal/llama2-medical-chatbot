3c47ad5e267af4180248a9dc8615c6caadde6db9
aditya-jindal <adityajindal1509@gmail.com>
2024-03-13 17:11:44 +0530

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Llama2 Medical Chatbot',
+    version= '0.0.0',
+    author= 'Aditya Jindal',
+    author_email= 'adityajindal1509.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
